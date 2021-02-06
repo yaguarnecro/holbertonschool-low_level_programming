b22064f72e44aa63579a48c8e3be87d84e871ea2
yaguarnecro <yaguarnecro@gmail.com>
2021-02-06 02:28:45 +0000

    add text

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,8 +17,8 @@ int main(void)
 	if (n > 0)
 		printf("%i is positive\n", n);
 	else if (n < 0)
-		printf("%i is zero\n", n);
-	else
 		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
 	return (0);
 }
