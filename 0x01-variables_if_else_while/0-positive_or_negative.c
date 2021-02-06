485f53e50d27826d3ff0c002082f0ee7b4a8ef13
yaguarnecro <yaguarnecro@gmail.com>
2021-02-06 02:02:17 +0000

    add %i to add variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+#include <stdio.h>
 /* betty style doc for function main goes there */
+
+/**
+ *  main - tells you if random number is negative or positive
+ *
+ *  Description: Uses if else to print if # if neg/pos/zero
+ *  Return: returns zero
+ */
+
 int main(void)
 {
 	int n;
@@ -12,12 +20,12 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		printf("is positive\n");
+		printf("%i is positive\n");
 	}if else(n==0)
 	{
-		printf("is zero\n");
+		printf("%i is zero\n");
 	}else{
-		printf("is negative\n");
+		printf("%i is negative\n");
 	}
 
 	return (0);
